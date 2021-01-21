ad4c153ee85cafdd02c5d84b1d82dd8c8175a546
kinsanghun <52707244+kinsanghun@users.noreply.github.com>
2021-01-21 23:24:00 +0900

    Update activation_func.py

--- activation_func.py
+++ activation_func.py
@@ -5,7 +5,19 @@ def step_func(x):
   y = x > 0
   return y.astype(np.int)
 
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x))
+
+
 x = np.arange(-5.0, 5.0, 0.1)
+
+# step_function
+y = step_func(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
+
+# sigmoid_function
 y = step_func(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
